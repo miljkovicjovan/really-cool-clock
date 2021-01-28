815df9c4bc787e4b7c79fbc030c1dee7de04d6e0
Jovan M <77690201+miljkovicjovan@users.noreply.github.com>
2021-01-28 12:55:23 +0100

    Add files via upload

--- cool clock/js/time.js
+++ cool clock/js/time.js
@@ -6,49 +6,42 @@ var evening = 18;
 var showCurrentTime = function()
 {
     var currentTime = new Date();
- 
+
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
     var meridian = "AM";
 
-    // Set hours
     if (hours >= noon)
     {
         meridian = "PM";
     }
-
     if (hours > noon)
     {
         hours = hours - 12;
     }
- 
-    // Set Minutes
+
     if (minutes < 10)
     {
         minutes = "0" + minutes;
     }
- 
-    // Set Seconds
     if (seconds < 10)
     {
         seconds = "0" + seconds;
     }
- 
-    // put together the string that displays the time
+
     var clockTime = hours + ':' + minutes + ':' + seconds + ' ' + meridian;
- 
+
+    
     document.getElementById("print-time").innerHTML = clockTime;
 };
 
-// Getting the clock to increment on its own and change out messages and pictures
 var updateClock = function() 
 {
     var time = new Date().getHours();
 
     var image = "images/day.jpg";
 
-    // Changing the background image during different parts of the day
     if (time > 6 && time < 12)
     {
         image = "images/morning.jpg";
@@ -71,9 +64,5 @@ var updateClock = function()
 };
 updateClock();
 
-
-
-
-// Getting the clock to increment once a second
 var oneSecond = 1000;
 setInterval( updateClock, oneSecond);
